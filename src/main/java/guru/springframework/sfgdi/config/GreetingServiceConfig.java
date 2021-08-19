ec64bac8df15b4830c5a2388ed6c9d25d52a18b3
Dmitry Kuksa <dkuksa@elinext.com>
2021-08-19 10:46:06 +0300

    feat: use spring boot application properties

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -16,12 +16,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
-
 
     @Bean
     FakeDataSource fakeDataSource(@Value("${guru.username}") String username,
